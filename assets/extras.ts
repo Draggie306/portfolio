28a3a7bf6301652f938ff632e463bd7522789e42
Draggie <75524491+Draggie306@users.noreply.github.com>
2025-11-01 22:37:01 +0000

    feat: add image optimisations

--- assets/extras.ts
+++ assets/extras.ts
@@ -51,12 +51,12 @@ function typeText(element: HTMLElement, charlist: string[]) {
 
 // Order: cycles every 5 seconds from top -> bottom, see function typeText
 const photos: string[] = [
-    "/assets/images/features/headshot.png",             // Main classic photo - awards
-    "/assets/images/features/djanogly.jpg",             // Outside Jubilee DLRC (notts)
-    "/assets/images/features/jess.png",                 // Inside Caius! (Cambs)
-    "/assets/images/features/computerphile-square.JPG", // near Play Button (notts)
-    "/assets/images/features/caribou-square.jpg",       // below Canadian Caribou
-    "/assets/images/features/kings-square.JPG",         // inside King's College (cambs)
+    "/assets/images/optimised/features/headshot.webp",             // Main classic photo - awards
+    "/assets/images/optimised/features/djanogly.webp",             // Outside Jubilee DLRC (notts)
+    "/assets/images/optimised/features/jess.webp",                 // Inside Caius! (Cambs)
+    "/assets/images/optimised/features/computerphile.webp", // near Play Button (notts)
+    "/assets/images/optimised/features/caribou.webp",       // below Canadian Caribou
+    "/assets/images/optimised/features/kings-square.webp",         // inside King's College (cambs)
 ];
 
 /**
