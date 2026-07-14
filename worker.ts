2336f254ce46d0d4d757d7b7654c3e48ea09ee06
Draggie <75524491+Draggie306@users.noreply.github.com>
2026-07-14 16:50:53 +0100

    feat: remove authentication
    Significantly improves and cleans the main worker evaluation code by removing the authentication pass.

--- worker.ts
+++ worker.ts
@@ -12,51 +12,9 @@
 export default {
     async fetch(request: Request, env: { ASSETS: { fetch: (arg0: string) => any; }; SUPER_SECRET_AUTH_TOKEN: string | null; }, ctx: any) {
         const url = new URL(request.url);
+
         const path = url.pathname;
-
-        if (path === "/login") {
-            const loginURL = new URL(request.url);
-            loginURL.pathname = "/protected/login.html";
-            return env.ASSETS.fetch(loginURL.toString());
-
-        }
-
-        // Also return early if path is part of the login
-        if (path === "/authenticate") {
-
-            // With a client-side GET request, the URL will contain ?auth_token=XXX. Parse this
-            const auth_token = url.searchParams.get("auth_token");
-
-            // Hardcoded for now
-            if (auth_token === env.SUPER_SECRET_AUTH_TOKEN || auth_token === env.SUPER_SECRET_AUTH_TOKEN2) {
-                return new Response(JSON.stringify(
-                    {
-                        "auth_token": env.SUPER_SECRET_AUTH_TOKEN,
-                        "status": 200,
-                    }
-                ));
-            } else {
-                console.log(`${auth_token} is not ${env.SUPER_SECRET_AUTH_TOKEN}!`)
-            }
-
-            // For all other cases, return 401
-            return new Response("Unauthorized", { status: 401 });
-        }
-
-        // If not a login request, check if there is a cookie with the auth_token
-        const auth_cookie = request.headers.get("Cookie")
-            ?.split("; ")
-            .find((row: string) => row.startsWith("auth_token="));
-
-        const given_token_value = auth_cookie ? auth_cookie.split("=")[1] : null;
-
-        if ((!given_token_value) || (given_token_value !== env.SUPER_SECRET_AUTH_TOKEN && given_token_value !== env.SUPER_SECRET_AUTH_TOKEN2)) {
-            console.log("cookie not entered or is invalid");
-            return await return_protected(request, env, 2);
-        }
-
-        // By now, it has passed all checks, so the user has logged in successfully before.
-        // Therefore, return their requested static asset.
+        console.log(`Request for path: ${path}`);
 
         return env.ASSETS.fetch(request.url);
     },
